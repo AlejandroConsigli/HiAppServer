699999929c5e6373df8a8bcdeb357bbe034a75a2
Alejandro <consiglialejandro@gmail.com>
2022-05-19 12:21:35 +0200

    Uploading code

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+app.use("/api/hi", require("./routes/hi"));
